d448e3ed46adffc78eb76326748f6d6774be47a0
ridwan414-hub <asifmahmoud414@gmail.com>
2024-06-10 14:56:44 +0600

    portfolio done

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+import { navItems } from '@/data';
+
+import Hero from '@/components/Hero';
+import Grid from '@/components/Grid';
+import RecentProjects from '@/components/RecentProjects';
+import Clients from '@/components/Cilients';
+import Experience from '@/components/Experience';
+import Approach from '@/components/Approach';
+import { FloatingNav } from '@/components/ui/FloatingNavbar';
+import Footer from '@/components/Footer';
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
